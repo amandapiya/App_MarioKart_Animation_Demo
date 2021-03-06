c073e6a045f36ca82ff4fad585bfb2b515e95d6c
amandapiya <apiyapan@caltech.edu>
2021-03-06 12:09:59 -0600

    Initial Commit

---
+++ App_MarioKart_Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_MarioKart_Animation
+//
+//  Created by Amanda P on 3/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
